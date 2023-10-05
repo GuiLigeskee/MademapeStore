5d648116ee95f1b89a8fe86b5d259482eb8a686f
GuiLigeskee <112131340+GuiLigeskee@users.noreply.github.com>
2023-10-05 10:55:43 -0300

    Atualizações dia 05/10/23 pt.1
    --Estilização da pagina quase completa;
    --A FAZER: service e slice dos buttons para terminar a funcionalidade da pagina;

--- frontend/src/pages/CreateButton/CreateButton.jsx
+++ frontend/src/pages/CreateButton/CreateButton.jsx
@@ -1,5 +1,5 @@
 // CSS
-import "./CreateButton.css";
+import styles from "./CreateButton.module.css";
 
 // Hooks
 import { useState } from "react";
@@ -23,6 +23,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const CreateButton = () => {
+  const dispatch = useDispatch();
+  // States
+  const [title, setTitle] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [buttonShape, setButtonShape] = useState("");
+  const [selectedButton, setSelectedButton] = useState("");
+  const [buttonColor, setButtonColor] = useState("#ffffff" || "#rrggbb");
+  const [titleColor, setTitleColor] = useState("#000000" || "#rrggbb");
+
+  const { loading, error } = useSelector((state) => state.auth);
+
+  // Imagens dos ícones
   const images = [
     Whatsapp,
     Instagram,
@@ -32,33 +44,136 @@ const CreateButton = () => {
     TikTok,
     Twitter,
     Youtube,
-  ]; // Substitua com os caminhos reais das suas imagens.
+  ];
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const goToPrevious = () => {
+  // Carrossel de icones
+  const goToPrevious = (e) => {
+    e.preventDefault();
     setCurrentIndex((prevIndex) =>
       prevIndex === 0 ? images.length - 1 : prevIndex - 1
     );
   };
 
-  const goToNext = () => {
+  const goToNext = (e) => {
+    e.preventDefault();
     setCurrentIndex((prevIndex) =>
       prevIndex === images.length - 1 ? 0 : prevIndex + 1
     );
   };
+
+  // Carrossel de formatos
+  const toggleButtonShape = (shape, e) => {
+    e.preventDefault();
+    setButtonShape(shape);
+    setSelectedButton(shape); // Defina o botão selecionado ao clicar
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aqui você pode processar o que foi selecionado, por exemplo, enviar para um servidor ou atualizar o estado do componente.
+    const button = {
+      title,
+      colorTitle,
+      backgroundColor,
+      format,
+      icon,
+      url,
+    };
+
+    console.log(user);
+
+    dispatch(register(user));
+  };
+
   return (
-    <div className="create-button">
-      <h2 id="titulo">Criar Botão</h2>
-      <div className="carousel">
-        <button className="carousel-button" onClick={goToPrevious}>
-          <FontAwesomeIcon icon={faArrowLeft} />
-        </button>
-        <img src={images[currentIndex]} alt={`Imagem ${currentIndex + 1}`} />
-        <button className="carousel-button" onClick={goToNext}>
-          <FontAwesomeIcon icon={faArrowRight} />
-        </button>
-      </div>
+    <div className={styles["create-button"]}>
+      <h2 id={styles["titulo"]}>Criar Botão</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>
+            Passo 1: <p>digite o nome do botão.</p>
+          </span>
+          <input type="text" placeholder="Nome do botão" />
+        </label>
+        <label>
+          <span>
+            Passo 2: <p>cole a URL desejada.</p>
+          </span>
+          <input type="url" placeholder="URL" />
+        </label>
+        <label>
+          <span>
+            Passo 3: <p>escolha um ícone para o seu botão.</p>
+          </span>
+          <div className={styles["carousel"]}>
+            <button
+              className={styles["carousel-button"]}
+              onClick={goToPrevious}
+            >
+              <FontAwesomeIcon icon={faArrowLeft} />
+            </button>
+            <img
+              src={images[currentIndex]}
+              alt={`Imagem ${currentIndex + 1}`}
+            />
+            <button className={styles["carousel-button"]} onClick={goToNext}>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </button>
+          </div>
+        </label>
+        <label>
+          <span>
+            Passo 4: <p>escolha o formato do botão.</p>
+          </span>
+          <div className={styles["button-shape-carousel"]}>
+            <button
+              type="button"
+              onClick={(e) => toggleButtonShape("circle", e)}
+              className={`${styles["button-shape-option"]} ${
+                selectedButton === "circle" ? styles["selected"] : ""
+              }`}
+            >
+              <div id={styles["shape-circle"]}></div>
+            </button>
+            <button
+              type="button"
+              onClick={(e) => toggleButtonShape("square", e)}
+              className={`${styles["button-shape-option"]} ${
+                selectedButton === "square" ? styles["selected"] : ""
+              }`}
+            >
+              <div id={styles["shape-long-circle"]}></div>
+            </button>
+          </div>
+        </label>
+        <label htmlFor="button-color" id="button-color">
+          <span>
+            Passo 5: <p>Escolher cor do botão.</p>
+          </span>
+          <input
+            type="color"
+            name="button-color"
+            id="button-color"
+            onChange={(e) => setButtonColor(e.target.value)}
+            value={buttonColor || ""}
+          />
+        </label>
+        <label htmlFor="title-color" id="title-color">
+          <span>
+            Passo 5: <p>Escolher cor do nome.</p>
+          </span>
+          <input
+            type="color"
+            name="title-color"
+            id="title-color"
+            onChange={(e) => setTitleColor(e.target.value)}
+            value={titleColor || ""}
+          />
+        </label>
+        {!loading && <input type="submit" value="Criar botão" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message msg={error} type="error" />}
+      </form>
     </div>
   );
 };
