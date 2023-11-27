45a5712fa6eb95df06de1b6a1540b1b6da938d29
Guiligeskee <guiligeskee@gmail.com>
2023-11-27 14:01:41 -0300

    .

--- frontend/src/utils/config.jsx
+++ frontend/src/utils/config.jsx
@@ -1,4 +1,5 @@
-export const api = "http://localhost:5000/api";
+// export const api = "http://localhost:5000/api";
+export const api = "https://mademape.onrender.com/api";
 export const uploads = "http://localhost:5000/uploads";
 
 export const requestConfig = (method, data, token = null, image = null) => {
